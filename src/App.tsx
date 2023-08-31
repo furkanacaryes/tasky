2cc273d53721ba16fdcae05764ef11f7dc4bdc41
CodeSandbox <support@codesandbox.io>
2023-08-31 18:15:09 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./global-styles.css";
+import { Draggable } from "./lib";
 
 function App() {
-  return null;
+  return (
+    <Draggable>
+      <div style={{backgroundColor: 'blue', width: 32, height: 32 }} />
+    </Draggable>
+  )
 }
 
 export default App;
--- src/lib/components/Draggable/index.tsx
+++ src/lib/components/Draggable/index.tsx
@@ -11,20 +11,13 @@ export const Draggable: FC<DraggableProps> = ({ children, className = "" }) => {
     <div
       draggable
       className={`draggable-container ${className}`}
-      onDrag={() => console.log("drag")}
-      onDragCapture={() => console.log("drag-capture")}
-      onDragStart={() => console.log("drag-start")}
-      onDragStartCapture={() => console.log("drag-start-capture")}
-      onDragEnter={() => console.log("drag-enter")}
-      onDragEnterCapture={() => console.log("drag-enter-capture")}
+      onDrag={(e) => e.preventDefault()}
+      // onDragStart={(e) => {e.preventDefault()}}
+      onDragEnter={(e) => e.preventDefault()}
       onDragEnd={() => console.log("drag-end")}
-      onDragEndCapture={() => console.log("drag-end-capture")}
       onDragExit={() => console.log("drag-exit")}
-      onDragExitCapture={() => console.log("drag-exit-capture")}
       onDragLeave={() => console.log("drag-leave")}
-      onDragLeaveCapture={() => console.log("drag-leave-capture")}
       onDragOver={() => console.log("drag-over")}
-      onDragOverCapture={() => console.log("drag-over-capture")}
     >
       {children}
     </div>
