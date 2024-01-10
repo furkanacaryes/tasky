12dafe4dfe3de60530d38d85067932bf037cdd00
Furkan Acar <furkan@hotelrunner.com>
2024-01-10 23:02:13 +0300

    feat: adds app icon

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  build: {
+    chunkSizeWarningLimit: 1024,
+    outDir: 'dist',
+    minify: false,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 })
